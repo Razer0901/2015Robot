2ac8aaa5f00d0ca684dec7a34d3abb5476eb1e0c
Geoffrey Wang <Geoffrey@BootCamp>
2015-02-01 22:40:30 -0500

    Encoders Fixed

--- RecycleRush/src/org/team5735/robot/Robot.java
+++ RecycleRush/src/org/team5735/robot/Robot.java
@@ -1,17 +1,28 @@
 package org.team5735.robot;
 
-import edu.wpi.first.wpilibj.*;
+import edu.wpi.first.wpilibj.CameraServer;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.VictorSP;
 import edu.wpi.first.wpilibj.buttons.Button;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
 
 public class Robot extends IterativeRobot {
-
+	
+	int loopCount = 0 ;
+	
 	RobotDrive driveTrain;
 	Joystick driveStick;
 	VictorSP liftMotor;
 	CameraServer server;
 	Encoder driveTrainRightEncoder;
 	Encoder driveTrainLeftEncoder;
+	Button testButton;
+	StatusOut statusOut;
 	
 	int driveTrainLeftMotorPort = 0;
 	int driveTrainRightMotorPort = 1;
@@ -24,8 +35,9 @@ public class Robot extends IterativeRobot {
 	int joystickLiftAxis = 2;
 	int joystickLiftUp = 5;
 	int joystickLiftDown = 6;
+	int joystickTestButton = 1;
 	
-	double driveTrainMoveScaling = 0.4;
+	double driveTrainMoveScaling = 0.5;
 	double driveTrainRotateScaling = 0.4;
 	
 	
@@ -37,18 +49,34 @@ public class Robot extends IterativeRobot {
 	
     public void robotInit() {
     	
-    	server = CameraServer.getInstance();
-        server.setQuality(50);
-        server.startAutomaticCapture("cam0");
+//    	server = CameraServer.getInstance();
+//        server.setQuality(50);
+//        server.startAutomaticCapture("cam0");
     	
     	driveTrain = new RobotDrive(driveTrainLeftMotorPort,driveTrainRightMotorPort);
     	driveStick = new Joystick(driveStickPort);
     	liftMotor = new VictorSP(liftMotorPort);
-    	
+    	testButton = new JoystickButton(driveStick,joystickTestButton);
+    	System.out.println("!!!!!!!!!!!!!!!!!!!!!!!");
+    	statusOut = new StatusOut(this);
+    	statusOut.start();
+    	System.out.println("&&&&&&&&&&&&&&&&&&&");
+
     	driveTrainRightEncoder = new Encoder(0, 1, false, Encoder.EncodingType.k4X);
     	driveTrainRightEncoder.reset();
-    	driveTrainLeftEncoder = new Encoder(0, 1, false, Encoder.EncodingType.k4X);
+    	driveTrainRightEncoder.setMaxPeriod(.1);
+    	driveTrainRightEncoder.setMinRate(10);
+    	driveTrainRightEncoder.setDistancePerPulse((6*Math.PI)/2048);
+    	driveTrainRightEncoder.setReverseDirection(true);
+    	driveTrainRightEncoder.setSamplesToAverage(7);
+    	
+    	driveTrainLeftEncoder = new Encoder(2, 3, false, Encoder.EncodingType.k4X);
     	driveTrainLeftEncoder.reset();
+    	driveTrainLeftEncoder.setMaxPeriod(.1);
+    	driveTrainLeftEncoder.setMinRate(10);
+    	driveTrainLeftEncoder.setDistancePerPulse((6*Math.PI)/2048);
+    	driveTrainLeftEncoder.setReverseDirection(false);
+    	driveTrainLeftEncoder.setSamplesToAverage(7);
 
     }
 
@@ -57,10 +85,28 @@ public class Robot extends IterativeRobot {
     }
 
     public void teleopPeriodic() {
+    	
         while (isOperatorControl() && isEnabled()){
+//        	loopCount ++;
+//        	if (loopCount > 200){
+//        		System.out.println(driveTrainLeftEncoder.get());
+//        		loopCount = 0;
+//        	}
+        	
+
+        	if(driveStick.getRawButton(joystickTestButton)){
+            	System.out.print("Left: ");
+           		System.out.println(driveTrainLeftEncoder.get());
+           		System.out.println(driveTrainLeftEncoder.getDistance());
+           		System.out.println();
+           		System.out.print("Right: ");
+            	System.out.println(driveTrainRightEncoder.get());
+            	System.out.println(driveTrainRightEncoder.getDistance());
+            	System.out.println();
+        	}
         	
         	if(driveStick.getRawButton(joystickLiftUp)){
-        		liftMotor.set(driveStick.getRawAxis(joystickLiftAxis));
+        		liftMotor.set(driveStick.getRawAxis(joystickLiftAxis)*1);
         	}else if(driveStick.getRawButton(joystickLiftDown)){
         		liftMotor.set(driveStick.getRawAxis(joystickLiftAxis)*-1);
         	}else{
@@ -69,6 +115,7 @@ public class Robot extends IterativeRobot {
         	
         	arcadeDrive(driveStick,joystickMoveAxis,driveTrainMoveScaling,joystickRotateAxis,driveTrainRotateScaling,joystickTurboAxis,true);
         	Timer.delay(0.01);
+        	
         }
     }
     
