97e769078b8f6aaf69f7864d7ed4f9635fd0ebb5
Geoffrey Wang <Geoffrey@BootCamp>
2015-02-08 22:56:31 -0500

    New Update

--- RecycleRush/src/org/team5735/robot/Robot.java
+++ RecycleRush/src/org/team5735/robot/Robot.java
@@ -6,9 +6,12 @@ import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.PIDController;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.VictorSP;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+import edu.wpi.first.wpilibj.command.Scheduler;
 
 public class Robot extends IterativeRobot {
 	
@@ -23,29 +26,51 @@ public class Robot extends IterativeRobot {
 	Encoder liftEncoder;
 	DigitalOutput ledGreen;
 	DigitalOutput ledRed;
+	PIDController liftPID;
+	GWJoystickButton testButton;
+	TestCommand testCommand;
 	
-	
+	//Computer Ports
 	int driveStickPort = 0;
+	
+	//PWM Ports
 	int driveTrainLeftMotorPort = 0;
 	int driveTrainRightMotorPort = 1;
 	int liftMotorPort = 2;
+	
+	//DIO Ports
+	int driveTrainRightEncoderPortA = 0;
+	int driveTrainRightEncoderPortB = 1;
+	int driveTrainLeftEncoderPortA = 2;
+	int driveTrainLeftEncoderPortB = 3;
+	int liftEncoderPortA = 4;
+	int liftEncoderPortB = 5;
 	int ledRedPort = 8;
 	int ledGreenPort = 9;
 	
-	int joystickMoveAxis = 5;
+	//Joystick Axis
 	int joystickRotateAxis = 0;
-	int joystickTurboAxis = 3;
 	int joystickLiftAxis = 2;
-	int joystickLiftUp = 5;
-	int joystickLiftDown = 6;
+	int joystickTurboAxis = 3;
+	int joystickMoveAxis = 5;
+	
+	//Joystick Buttons
 	int joystickTestButton = 1;
 	int joystickResetButton = 2;
 	int joystickLedButton = 3;
+	int joystickLiftUp = 5;
+	int joystickLiftDown = 6;
 	
+	//DriveTrainScaling
 	double driveTrainMoveScaling = 0.5;
 	double driveTrainRotateScaling = 0.5;
 	
+	//PID Values
+	double liftP = 0.1;
+	double liftI = 0.01;
+	double liftD = 0.1;
 	
+	//Redeclaration of arcadeDrive
     public void arcadeDrive(GenericHID joystick, final int moveAxis, final double moveScaling, final int rotateAxis, final double rotateScaling, final int turboAxis, boolean squaredInputs){
     	double moveValue = joystick.getRawAxis(moveAxis)*(joystick.getRawAxis(turboAxis)+1)*moveScaling;
     	double rotateValue = joystick.getRawAxis(rotateAxis)*rotateScaling;
@@ -53,40 +78,69 @@ public class Robot extends IterativeRobot {
     }
 	
     public void robotInit() {
-    	
-    	
+    	/*
+    	//Init Webcam
     	camServer = CameraServer.getInstance();
         camServer.setQuality(50);
         camServer.startAutomaticCapture("cam0");
+    	*/
     	
+        //Create RobotDrive
     	driveTrain = new RobotDrive(driveTrainLeftMotorPort,driveTrainRightMotorPort);
+    	
+    	//Create Joystick
     	driveStick = new Joystick(driveStickPort);
     	
+    	//Create DIOs
     	ledGreen = new DigitalOutput(ledGreenPort);
     	ledRed = new DigitalOutput(ledRedPort);
     	
+    	//Create MotorController
     	liftMotor = new VictorSP(liftMotorPort);
-
-    	driveTrainRightEncoder = new Encoder(0, 1, false, Encoder.EncodingType.k4X);
+    	
+    	//Set Up Right Encoder
+    	driveTrainRightEncoder = new Encoder(driveTrainRightEncoderPortA, driveTrainRightEncoderPortB, true, Encoder.EncodingType.k4X);
     	driveTrainRightEncoder.reset();
     	driveTrainRightEncoder.setMaxPeriod(.1);
     	driveTrainRightEncoder.setMinRate(10);
-    	driveTrainRightEncoder.setDistancePerPulse(-1*(6*Math.PI)/2048*125/120);
+    	driveTrainRightEncoder.setDistancePerPulse((6*Math.PI)/2048*125/120);
     	driveTrainRightEncoder.setSamplesToAverage(10);
     	
-    	driveTrainLeftEncoder = new Encoder(2, 3, false, Encoder.EncodingType.k4X);
+    	//Set up Left Encoder
+    	driveTrainLeftEncoder = new Encoder(driveTrainLeftEncoderPortA, driveTrainLeftEncoderPortB, false, Encoder.EncodingType.k4X);
     	driveTrainLeftEncoder.reset();
     	driveTrainLeftEncoder.setMaxPeriod(.1);
     	driveTrainLeftEncoder.setMinRate(10);
     	driveTrainLeftEncoder.setDistancePerPulse((6*Math.PI)/2048*125/120);
     	driveTrainLeftEncoder.setSamplesToAverage(10);
-
+    	
+    	/*
+    	//Set up Lift Encoder
+    	liftEncoder = new Encoder(liftEncoderPortA, liftEncoderPortB, false, Encoder.EncodingType.k4X);
+    	liftEncoder.reset();
+    	liftEncoder.setMaxPeriod(.1);
+    	liftEncoder.setMinRate(10);
+    	liftEncoder.setDistancePerPulse((6*Math.PI)/2048*125/120);
+    	liftEncoder.setSamplesToAverage(10);
+    	*/
+    	
+    	/*
+    	//Set up PID
+    	liftPID = new PIDController (liftP, liftI, liftD, liftEncoder, liftMotor);
+    	liftPID.enable();
+    	*/
+    	
+    	testButton = new GWJoystickButton(driveStick, 4);
+    	testCommand = new TestCommand();
+    	System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!");
     }
 
     public void autonomousPeriodic() {
     	while(isAutonomous()&& isEnabled()){
+    		driveTrainRightEncoder.reset();
+    		driveTrainLeftEncoder.reset();
+    		
     		if (loopCount == 100){
-    			
     			while(driveTrainLeftEncoder.getDistance() <30 && driveTrainRightEncoder.getDistance() <30){
     				driveTrain.drive(-0.25, 0);
     				System.out.println("Running While loop");
@@ -138,15 +192,7 @@ public class Robot extends IterativeRobot {
     }
 
     public void teleopPeriodic() {
-    	
         while (isOperatorControl() && isEnabled()){
-//        	loopCount ++;
-//        	if (loopCount > 200){
-//        		System.out.println(driveTrainLeftEncoder.get());
-//        		loopCount = 0;
-//        	}
-        	
-
         	if(driveStick.getRawButton(joystickLedButton)){
             	ledGreen.set(true);
             	ledRed.set(false);
@@ -154,6 +200,8 @@ public class Robot extends IterativeRobot {
         		ledGreen.set(false);
         		ledRed.set(true);
         	}
+        	
+        	testButton.whenPressed(testCommand);
         	
         	if(driveStick.getRawButton(joystickTestButton)){
             	System.out.print("Left: ");
@@ -182,6 +230,8 @@ public class Robot extends IterativeRobot {
         	Timer.delay(0.01);
         	
         }
+        System.out.println("****************************************");
+    	Scheduler.getInstance().run();
     }
     
 
